e95b945a52aecbd11d7974279f7a2f96d13f3f52
Adam Herman <adm.herman@gmail.com>
2024-08-31 18:11:17 +0200

    fix to prevent unneccesary citation downloads

--- src/utils/citation.svelte.ts
+++ src/utils/citation.svelte.ts
@@ -16,34 +16,35 @@ export type Author = {
 	family: string;
 };
 
-export let litarature: Map<string, Citation> = new Map<string, Citation>(); // key: DOI, value: Citation
+const literature: Map<string, Promise<Citation>> = new Map<string, Promise<Citation>>();
 
 export async function getCitationInfo(DOI: string): Promise<Citation> {
-	if (litarature.has(DOI)) {
-		return litarature.get(DOI);
+	if (!literature.has(DOI)) {
+		const citationPromise = fetchCitation(DOI);
+		literature.set(DOI, citationPromise);
 	}
+	return literature.get(DOI)!;
+}
 
+async function fetchCitation(DOI: string): Promise<Citation> {
 	try {
 		const cite = await Cite.async(DOI);
 		const data = cite.data[0];
 
 		console.log('data:', data);
 
-		let citation: Citation = {
+		return {
 			DOI: DOI,
-			authors: data.author?.map((author: any) => {
-				return { given: author.given, family: author.family };
-			}),
+			authors: data.author?.map((author: any) => ({
+				given: author.given,
+				family: author.family
+			})),
 			created: new Date(data.created['date-time']),
 			title: data.title,
-			subtitle: data.subtitle[0],
+			subtitle: data.subtitle?.[0],
 			abstract: data.abstract,
 			URL: data.URL
 		};
-
-		litarature.set(DOI, citation);
-
-		return citation;
 	} catch (error) {
 		console.error('Error fetching citation:', error);
 		throw error;
